e8711282c9150f6e32fb0e7bcb3108dc4294fde5
maninipuranik <manini.puranik@gmail.com>
2013-12-20 12:32:53 +0530

    Loading the log4j.properties from res folder and not classpath

--- src/com/cs/loadbalancer/indesign/helpers/DefaultLoggerImpl.java
+++ src/com/cs/loadbalancer/indesign/helpers/DefaultLoggerImpl.java
@@ -1,6 +1,7 @@
 package com.cs.loadbalancer.indesign.helpers;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 
 public class DefaultLoggerImpl {
@@ -8,6 +9,7 @@ public class DefaultLoggerImpl {
 	protected Logger debugLogger;
 	
 	public DefaultLoggerImpl(String loggerName) {
+		PropertyConfigurator.configure("res/log4j.properties");
 		debugLogger = Logger.getLogger(loggerName);
 	}
 	
